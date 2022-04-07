651fb7728e70b4fed000c6e6a885b1a87a067b3c
Santiago Salazar <santiagosalazar.dev@gmail.com>
2022-04-07 16:02:22 -0400

    Hello, bro x2

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
-﻿using System.Reflection;
+﻿using System.Data.Common;
+using System.Xml;
+using System.Data.SqlTypes;
+using System.Runtime.CompilerServices;
+using System.Data;
+using System.Globalization;
 using System.ComponentModel;
+using System.Runtime.Serialization;
 using System;
+using System.Reflection;
 using System.Text;
 using System.Linq;
 using System.Date;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Threading.Tasks;
+
 
 
 namespace DateTime
@@ -16,20 +23,22 @@ namespace DateTime
         static void Main(string[] args)
         {
 
+            // Console.WriteLine("Jodete Using");
+
             CultureInfo cultureinfoUSA = new CultureInfo("en-us");
             DateTime dateTime = new DateTime(10,22,2015);
             DateTime ejemplo1 = Convert.ToDateTIme("04/07/2022 13:50:30am", cultureinfoUSA);
             DateTime ejemplo2 = DateTime.Parse("04/07/2022 13:50:30am", cultureinfoUSA);
 
-
+            
             Console.WriteLine("Dia " + ejemplo1.Day);
             Console.WriteLine("Mes " + ejemplo1.Month);
-            Console.WriteLine("A;o " + ejemplo1.Year);
+            Console.WriteLine("Año " + ejemplo1.Year);
             Console.WriteLine("Hora " + ejemplo1.Hour);
             Console.WriteLine("Minuto " + ejemplo1.Minute);
             Console.WriteLine("Segundo " + ejemplo1.Second);
             Console.WriteLine("Dia de la semana " + ejemplo1.DayOfWeek);
-            Console.WriteLine("Dia del a;o " + ejemplo1.DayOfYear);
+            Console.WriteLine("Dia del año " + ejemplo1.DayOfYear);
             Console.WriteLine("Tiempo " + ejemplo1.Date);
 
 
